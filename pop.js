29d1534cb34a61404a22077e11e5d0691ff15f8e
WON-DER <wonderfilledyt@gmail.com>
2021-05-12 09:37:27 -0700

    added pop.js

---
+++ pop.js
@@ -0,0 +1,11 @@
+// pop removes the last element from an array and returns that element. This method changes the length of the array.
+
+// pop takes no parameters.
+
+// pop returns the element from the array, undefined if array is empty.
+
+// 1. Remove an element
+
+const arr = [3, 4, 1];
+
+const gimme = arr.pop();
