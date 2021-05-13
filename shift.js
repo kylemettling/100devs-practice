1440c58e4ddd1401c606e41f99afc3c3d6dc2813
WON-DER <wonderfilledyt@gmail.com>
2021-05-13 09:40:33 -0700

    added shift.js

---
+++ shift.js
@@ -0,0 +1,9 @@
+// shift removes the first element from an array and returns the removed element.
+
+// this method changes the length of the array.
+
+// shift takes no parameters.
+
+// 1. Remove leading element
+const arr = [2, 3, 1, 2];
+const first = arr.shift();
