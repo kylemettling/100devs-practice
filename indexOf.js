62bd5386b48f3c9adf1d1808b1c262164ba39fff
WON-DER <wonderfilledyt@gmail.com>
2021-05-16 09:58:37 -0700

    added indexOf.js

---
+++ indexOf.js
@@ -0,0 +1,8 @@
+// indexOf returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+// indexOf takes two parameters: searchElement - element to locate, fromIndex (optional) - the index to start the search at. If the index is greater than or equal to the array's length, -1 is returned, which means the array will not be searched.
+
+// 1. Return index
+
+const arr = [2, 3, 1, 2, 3];
+console.log(arr.indexOf(3));
